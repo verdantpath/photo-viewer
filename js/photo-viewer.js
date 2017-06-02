ab4077db4da6281a0a7a26e1faf03914c584a8f3
Matt <m_dittmann@yahoo.com>
2017-06-01 22:23:02 -0700

    update js

--- js/photo-viewer.js
+++ js/photo-viewer.js
@@ -3,3 +3,19 @@ var $current;
 var cache = {};
 var $frame = $('#photo-viewer');
 var $thumbs = $('.thumb');
+
+function crossfade($img) {
+
+  if ($current){
+    $current.stop().fadeOut('slow');
+  }
+
+  $img.css({
+      marginLeft: -$img.width() / 2,
+      marginTop: -$img.height() / 2
+  });
+
+  $img.stop().fadeTo('slow', 1);
+
+  $current = $img;
+}
